5df20e6dce395dd2b81020b43d72b45bb4c4890a
LexyNyan <ifoggyelf@gmail.com>
2022-06-26 20:48:06 +0600

    first commit

---
+++ ConsoleApp1/CheckSumTo30.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    
+    public class CheckSumTo30
+    {
+        public int firstNumber { private get; set; }
+        public int secondNumber { private get; set; }
+
+        public void CheckSum()
+        {
+            if (firstNumber < 1)
+            {
+                Console.WriteLine("Enter the first number");
+                firstNumber = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the second number");
+                secondNumber = Int32.Parse(Console.ReadLine());
+            }
+
+            Print((firstNumber == 30 || secondNumber == 30) 
+                   || (firstNumber + secondNumber == 30));
+        }
+
+        public void CheckSumOfRandom()
+        {
+            Console.WriteLine("Enter count of couples numbers");
+            int coplesCount = Int32.Parse(Console.ReadLine());
+            Random random = new Random();
+            for (int i = 0; i < coplesCount; i++)
+            {
+                firstNumber = random.Next(1, 30);
+                secondNumber = random.Next(1, 30);
+                CheckSum();
+            }
+        }
+
+        private void Print(bool value)
+        {
+            if(value) Console.WriteLine(firstNumber + "+" + secondNumber
+                                        + " Sum of numbers equal 30");
+            else Console.WriteLine(firstNumber + "+" + secondNumber 
+                                   + " Sum of numbers not equal 30");
+        }
+        
+    }
+}
---
+++ ConsoleApp1/program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckSumTo30 CheckSum30 = new();
+            Console.WriteLine("This program checking a sum of two numbers. " +
+                              "\nEnter 1 if you want write numbers yourself \n" +
+                              "or enter 2 to write random numbers.");
+            int choice =  Int32.Parse(Console.ReadLine());
+            if (choice < 2) CheckSum30.CheckSum();
+            else CheckSum30.CheckSumOfRandom();
+        }
+    }
+}
